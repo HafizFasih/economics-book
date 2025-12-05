058b843101ebf9871994e5ddfb63cb62cd6f1e92
Muhammad Fasih <programmer.fasih.18@gmail.com>
2025-12-05 05:35:51 +0500

    .

--- economics-book/src/pages/index.tsx
+++ economics-book/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/demand-and-supply">
-            Docusaurus Tutorial - 5min ⏱️
+            familiar with book
           </Link>
         </div>
       </div>
